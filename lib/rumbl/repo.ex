961a6ffa4a059558310f5bcdc6e2958e18ffd2ec
Dennis Zelada <denniszelada@Denniss-MBP.home>
2018-05-14 16:49:08 +0200

    Initial commit

---
+++ lib/rumbl/repo.ex
@@ -0,0 +1,15 @@
+defmodule Rumbl.Repo do
+  # use Ecto.Repo, otp_app: :rumbl
+  @moduledoc """
+  In memory repository.
+  """
+
+  def all(Rumbl.User) do
+    [%Rumbl.User{id: "1", name: "Jose", username: "josevalim", password: "elixir"},
+     %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
+     %Rumbl.User{id: "3", name: "Chris", username: "chirsmccord", password: "phx"},
+    ]
+  end
+
+  def all(_module), do: []
+end
